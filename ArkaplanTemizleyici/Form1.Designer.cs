d1d448cd416cf077b984a05c9511b3fa922a72d9
J <webmanyak@gmail.com>
2020-09-22 21:53:07 -0700

    Initial commit

---
+++ ArkaplanTemizleyici/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ArkaplanTemizleyici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 121);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag && Drop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 137);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Background Remover";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ArkaplanTemizleyici/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkaplanTemizleyici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string adres = string.Empty;
+        string[] dosya = new string[1];
+        public static string DosyaAdresi = string.Empty;
+        string[] _surukleBirakDosyasi = new string[1];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void label1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                //Drag drop
+                _surukleBirakDosyasi = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                foreach (string surukleBirak in _surukleBirakDosyasi)
+                    DosyaAdresi = surukleBirak;
+                //Drag drop
+
+                //Api 
+                using (var client = new HttpClient())
+                using (var formData = new MultipartFormDataContent())
+                {
+                    formData.Headers.Add("X-Api-Key", "1nZm2g5D3tgvJ5jEYcdRt8iq");
+                    formData.Add(new ByteArrayContent(File.ReadAllBytes(DosyaAdresi)), "image_file", DosyaAdresi);
+                    formData.Add(new StringContent("auto"), "size");
+                    var response = client.PostAsync("https://api.remove.bg/v1.0/removebg", formData).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        FileStream fileStream = new FileStream("no-bg.png", FileMode.Create, FileAccess.Write, FileShare.None);
+                        response.Content.CopyToAsync(fileStream).ContinueWith((copyTask) => { fileStream.Close(); });
+                        MessageBox.Show("Arkaplanı temizlenmiş dosyayı programın bulunduğu klasörde bulabilirsiniz.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        Process.Start(Application.StartupPath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Hata: " + response.Content.ReadAsStringAsync().Result);
+                    }
+                }
+                //Api 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
